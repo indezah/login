e6ed65ce3c8ff499f7509e4308bd686821bb0024
Nisura Indisa <64645496+indezah@users.noreply.github.com>
2022-11-22 14:22:16 +0530

    More commits

--- app/controllers/Signup2.php
+++ app/controllers/Signup2.php
@@ -4,11 +4,11 @@ class Signup2 extends Controller
 {
     public function index()
     {
-        $user = new employeeSupplier();
+        $user = new Employee();
         if ($user->validate($_POST)) {
-      
             $user->insert($_POST);
-        }
+        } 
+        
         show($user->errors);
         show($_POST);
         $this->view('signup2');
